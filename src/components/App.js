1d991cbdc7c58870ada3f814f12e0c9b7b406dd8
Sarah Young <sarahyoung@Sarahs-MBP.localdomain>
2019-01-17 11:59:33 +0100

    Finished tutorial project.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
-const App = () => {
 
-    return (
+class App extends React.Component {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: {query: term},   
+    });
+
+    this.setState({ images: response.data.results })
+}
+
+    render() {
+        return (
         <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-        )
-    }
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images}/>
+      </div>
+    )
+  }
+}
+
 
 export default App;
+
+//https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+//Authorization: Client-ID YOUR_ACCESS_KEY
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-    onInputChange() {
-        
-    }
+    state = {term: ''}
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term)
+    };
 
     render() {
         return (
         <div className="ui segment">
-            <form className="ui form"> 
+            <form onSubmit={this.onFormSubmit} className="ui form"> 
                 <div className="field">
                 <label>Image Search</label>
-                <input type="text" />
+                    <input 
+                        type="text" 
+                        value={this.state.term} 
+                        onChange={(e) => this.setState({ term: e.target.value })} 
+                    /> 
                 </div>
             </form>
         </div>)
@@ -20,3 +27,6 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+// () are left off of the end of the onInputChange callback in the JSX so the function isn't called everytime it is rendered.
+// Arrow functions work as a secondary way to "bind this"
